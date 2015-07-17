87fc001f0901867125d0ad0d1df6fe837fc75da4
aung-barto <aungwaran@gmail.com>
2015-07-16 21:09:14 -0400

    post page

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,5 +2,8 @@ class PostsController < ApplicationController
 	def index
 	end
 	def show
+    @post = Post.find(params[:id])
+    @location = Location.find(@post.location_id)
+    @category = Category.find(@post.category_id)
 	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root 'home#index'
 
   #search for home
-  # get 'home#search'
+  get 'search' => 'home#search'
 
   resources :category
   resources :posts
