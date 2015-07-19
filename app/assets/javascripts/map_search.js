0b80f9602f35e09f97b508e7e708e599c3cf6372
aung-barto <aungwaran@gmail.com>
2015-07-19 11:41:26 -0400

    fixed seed file with new images

--- app/assets/javascripts/map_search.js
+++ app/assets/javascripts/map_search.js
@@ -6,7 +6,7 @@ var map;
 var results;
 var address;
 var address2 = [];
-
+var latLngs = [] 
 function initialize(){
    // grabbing address from location input
   
@@ -16,34 +16,44 @@ function initialize(){
   // };
 
   $('body').find('span').each(function(){
-    address2.push(this.html());
-    console.log(this.html());
+    address2.push(this.innerHTML);
   });
+  
+  var geocoder = new google.maps.Geocoder();
   for (i = 0; i < address2.length; i++){
     address = address2[i];
+    console.log(address);
+    geocoder.geocode({ 'address': address}, function(results, status){
+      latLngs.push(results[0].geometry.location);
+    });
   };
-  console.log(address);
 
-  var geocoder = new google.maps.Geocoder();
-  geocoder.geocode({ 'address': address}, function(results, status){
+
+if (latLngs.length > 200){
+  // var geocoder = new google.maps.Geocoder();
+  // geocoder.geocode({ 'address': address}, function(results, status){
 
     if (status == google.maps.GeocoderStatus.OK){
+      for(j = 0; j< latLngs.length; j++){
     var mapOptions = {
       // use current location as the center
-      center: new google.maps.LatLng(results[0].geometry.location),
-      zoom: 15,
-      mapTypeId: google.maps.MapTypeId.ROADMAP
-      };
+        center: new google.maps.LatLng(latLngs[j]),
+        zoom: 15,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+        };
 
-      map = new google.maps.Map($('#map-canvas')[0], mapOptions);
-      map.setCenter(results[0].geometry.location);
-      var marker = new google.maps.Marker({
-        animation: google.maps.Animation.DROP,
-        map: map,
-        position: results[0].geometry.location
-      });
+        map = new google.maps.Map($('#map-canvas')[0], mapOptions);
+        map.setCenter(results[0].geometry.location);
+
+        var marker = new google.maps.Marker({
+          animation: google.maps.Animation.DROP,
+          map: map,
+          position: latLngs[j]
+        });
+      }
     } else {
       alert("Geocode was not successful for the following reason: " + status);
     }
-  });
+  // });
+}
 }
