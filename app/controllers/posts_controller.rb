73461de7698353eab1711cee78e62d2d4d04c765
aung-barto <aungwaran@gmail.com>
2015-07-20 14:32:33 -0400

    recommendations on post page works

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,16 +6,19 @@ class PostsController < ApplicationController
 	end
 
 	def show
-    @posts = Post.all
     @post = Post.find(params[:id])
     @location = Location.find(@post.location_id)
     @category = Category.find(@post.category_id)
+
+    # get similar posts
+    @posts = Post.where(category_id: @category.id).where.not(id: params[:id]).sample(4)
 	end
   
   def search
-    query = params[:name]
-    @results = Post.search(query).limit(10)
+    @query = params[:name]
+    @results = Post.search(@query).limit(10)
 
+    # get address to be convert into lat/lng for map markers
     @locations = []
     @results.each do |r|
       @locations << Location.find(r.location_id)
